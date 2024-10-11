3b20bd473c72cd6070f757644cec3ddc528f82e4
Juan Camilo Tenorio Munoz <1193225773@u.icesi.edu.co>
2024-10-11 07:28:31 -0500

    add_Scaffholding2

--- server/app.js
+++ server/app.js
@@ -7,9 +7,40 @@ const app = express() // Creates HTTP server
 app.use(express.json()) // utility to process JSON in requests
 app.use(cors()) // utility to allow clients to make requests from other hosts or ips
 
+const path = require("path");
+
+const clientTvPath = path.resolve(__dirname, "../client-tv");
+const clientMobilePath = path.resolve(__dirname, "../client-mobile");
+app.use('/node_modules', express.static(path.join(__dirname, '../node_modules')));
+
+// Serve Client App 1
+app.use("/tv", express.static(clientApp1Path));
+
+// Serve Client App 2
+app.use("/mobile", express.static(clientApp2Path));
+
+// Catch-all route for Client App 1
+app.get("/tv/*", (req, res) => {
+  res.sendFile(path.join(clientTvPath, "index.html"));
+});
+
+// Catch-all route for Client App 2
+app.get("/mobile/*", (req, res) => {
+  res.sendFile(path.join(clientMobilePath, "index.html"));
+});
+
+const usersRouter = require("./routes/users")
+
+app.use("/", usersRouter)
+
+module.exports = app;
+
+/*
 const usersRouter = require("./routes/users")
 
 app.use("/", usersRouter)
 //son endpoints, se controlan con los controladores en lugar de los handlers
 //son requests
 module.exports = app
+
+*/
