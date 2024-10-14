8308c4d945f6fa5c680c778a8cc8239aec08fd50
valegaru <valegaru2003@gmail.com>
2024-10-14 17:48:52 -0500

    screen1 to 2 working

--- client-tv/screens/1waitingScreen.js
+++ client-tv/screens/1waitingScreen.js
@@ -1,7 +1,4 @@
 import { router, socket } from '../routes.js';
-//import socket from '../socket.js';
-//import prueba from '../routes.js';
-
 
 export default function render1waitingScreen() {
 	const app = document.getElementById('app');
@@ -12,24 +9,12 @@ export default function render1waitingScreen() {
 				<p>ejemplo</p>
     `;
 
-	// Botón para emitir un evento manualmente desde el cliente (para pruebas)
-	document.getElementById('emitButton').addEventListener('click', () => {
-		console.log('emited');
-		socket.emit('event1', { message: 'Hello from About page' });
-	});
-
-	// Botón para cambiar manualmente a la segunda screen (para pruebas)
-	document.getElementById('goToScreen2').addEventListener('click', () => {
-		router.navigateTo('/screen2');
-	});
-
 	// Escuchar el evento "sensorActivated" desde el servidor
 	socket.on('sensorActivated', () => {
-		console.log('sensorActivated event received, navigating to screen 2');
-		// Cambiar a la segunda screen automáticamente cuando el evento se reciba
-		router.navigateTo('/screen2');
+		console.log('Evento sensorActivated recibido, cambiando a la pantalla 2');
+		router.navigateTo('/screen2'); // Cambiar a la pantalla 2 automáticamente
 	});
 }
 
-//listen for presenceToServer arduino event que deberia ser un endpoint
-// cambia de screen
+//listen for presenceToServer endpoint
+// cambia de screen a 2welcomeScreen.
