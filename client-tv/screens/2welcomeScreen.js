a6dcb3baed152d202e0996c959c900fd5ed1e60b
valegaru <valegaru2003@gmail.com>
2024-10-14 18:06:47 -0500

    camera activated screen 2

--- client-tv/screens/2welcomeScreen.js
+++ client-tv/screens/2welcomeScreen.js
@@ -5,39 +5,22 @@ export default function render2welcomeScreen() {
 	app.innerHTML = `
 	<p>qr</p>
         <p>Para iniciar la experiencia correctamente, con tu celular lee este codigo QR</p>
-
+	  <video id="cameraFeed" autoplay playsinline style="width: 100%; height: auto;"></video> <!-- Video de la cámara -->
     `;
 
-	async function requestListOfUsers() {
+	// Función para encender la cámara
+	const startCamera = async () => {
 		try {
-			const url = 'http://localhost:5050/users';
-			const response = await fetch(url);
-			if (!response.ok) {
-				throw new Error(`HTTP error! Status: ${response.status}`);
-			}
-			const data = await response.json();
-
-			return data;
-		} catch (error) {
-			console.error('Fetch error:', error);
+			// Acceder al stream de video de la cámara
+			const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+			const videoElement = document.getElementById('cameraFeed');
+			videoElement.srcObject = stream;
+		} catch (err) {
+			console.error('Error al acceder a la cámara: ', err);
+			app.innerHTML += `<p>Error al acceder a la cámara: ${err.message}</p>`;
 		}
-	}
+	};
 
-	document.getElementById('requestButton').addEventListener('click', async () => {
-		const listUsers = await requestListOfUsers();
-		console.log(listUsers);
-	});
-
-	document.getElementById('goToScreen1').addEventListener('click', () => {
-		router.navigateTo('/');
-		socket.emit('event2');
-	});
-
-	document.getElementById('goToScreen3').addEventListener('click', () => {
-		router.navigateTo('/screen3');
-	});
+	// Llamar a la función para iniciar la cámara cuando llegamos a esta pantalla
+	startCamera();
 }
-
-//activa la camara
-//listen for mobile event socket userConnectedServe
-//next screen
