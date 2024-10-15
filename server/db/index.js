c957078bfad3c7c0d10a69e1eac781217fc9a6ea
valegaru <valegaru2003@gmail.com>
2024-10-15 15:48:47 -0500

    db question update

--- server/db/index.js
+++ server/db/index.js
@@ -22,12 +22,32 @@ const questions = [
 	},
 	{
 		id: 2,
-		question: '¿Cuál es tu mayor motivación para crecer?',
+		question: '¿Qué hábito te gustaría incorporar en tu rutina diaria para el 2025?',
 		options: [
-			{ option: 'A. Lograr mis objetivos personales', image: 'opciones/2opA.png' },
-			{ option: 'B. Inspirar a otros', image: 'opciones/2opB.png' },
-			{ option: 'C. Superar mis miedos', image: 'opciones/2opC.png' },
-			{ option: 'D. Encontrar mi propósito en la vida', image: 'opciones/2opD.png' },
+			{ option: 'A. Hacer ejercicio regularmente', image: 'opciones/2opA.png' },
+			{ option: 'B. Llevar una alimentación más saludable', image: 'opciones/2opB.png' },
+			{ option: 'C.  Aprender algo nuevo (un idioma, una habilidad)', image: 'opciones/2opC.png' },
+			{ option: 'D. Meditar, practicar mindfulness o journaling', image: 'opciones/2opD.png' },
+		],
+	},
+	{
+		id: 3,
+		question: '¿Cuál de estas afirmaciones te resuena para tu mejor versión del 2025?',
+		options: [
+			{ option: 'A. Estoy en paz y me cuido a mí misma', image: 'opciones/3opA.png' },
+			{ option: 'B. Soy exitosa y estoy cumpliendo mis metas', image: 'opciones/3opB.png' },
+			{ option: 'C.  Vivo la vida con alegría y creatividad', image: 'opciones/3opC.png' },
+			{ option: 'D. Soy una persona equilibrada y en crecimiento constante', image: 'opciones/3opD.png' },
+		],
+	},
+	{
+		id: 4,
+		question: '¿Cuáles son tus vacaciones soñadas para el 2025? ',
+		options: [
+			{ option: 'A. Viaje a la playa', image: 'opciones/4opA.png' },
+			{ option: 'B. Cabaña privada en medio de las montañas', image: 'opciones/4opB.png' },
+			{ option: 'C.  Eurotrip', image: 'opciones/4opC.png' },
+			{ option: 'D. Aventura en medio de la naturaleza', image: 'opciones/4opD.png' },
 		],
 	},
 	// Agrega más preguntas aquí
