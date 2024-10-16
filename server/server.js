b3392a9fc9a757eb703c227c36299ce01d51f89d
SofiRn <sofiaromeronar@gmail.com>
2024-10-16 08:48:57 -0500

    prompt-answers

--- server/server.js
+++ server/server.js
@@ -39,6 +39,8 @@ const httpServer = createServer(app); // Explicity creates an HTTP server from t
 // Initialize Socket.IO
 initSocket(httpServer);
 
+
+
 httpServer.listen(5050, () => console.log('server starting 🚀🆙✔ on http://localhost:5050'));
 
 //para importar la base de datos
