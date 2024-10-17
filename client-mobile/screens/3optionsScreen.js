f58b5024f6bcdf00216a1ff22718a05c0fca860d
JuanTenorioMunoz <tenorio2003@gmail.com>
2024-10-17 15:02:45 -0500

    minorFix

--- client-mobile/screens/3optionsScreen.js
+++ client-mobile/screens/3optionsScreen.js
@@ -3,34 +3,34 @@ import { router, socket } from '../routes.js';
 export default function renderOptionsScreen() {
 	const app = document.getElementById('app');
 	let answer = '';
-	let questionCounter = 0; // Cambiado a 0 para empezar desde la primera pregunta
+	let questionCounter = 0; 
 	let continueEnable = false;
-	let questions = []; // Almacenamos las preguntas aquí
+	let questions = []; 
 
-	// Función para renderizar las opciones de la pregunta actual
+
 	function renderCurrentOptions() {
 		if (questions.length > 0 && questionCounter < questions.length) {
 			const currentQuestion = questions[questionCounter];
 			console.log("current" + currentQuestion)
-			// Renderizamos las opciones en los botones A, B, C y D
+
 			document.querySelector('.option#buttonA').textContent = currentQuestion.options[0].option;
 			document.querySelector('.option#buttonB').textContent = currentQuestion.options[1].option;
 			document.querySelector('.option#buttonC').textContent = currentQuestion.options[2].option;
 			document.querySelector('.option#buttonD').textContent = currentQuestion.options[3].option;
 
-			// Guardar los valores en atributos de los botones para su uso posterior
+
 			document.querySelector('.option#buttonA').value = currentQuestion.options[0].option;
 			document.querySelector('.option#buttonB').value = currentQuestion.options[1].option;
 			document.querySelector('.option#buttonC').value = currentQuestion.options[2].option;
 			document.querySelector('.option#buttonD').value = currentQuestion.options[3].option;
 
-			console.log(`Pregunta ${questionCounter + 1}: ${currentQuestion.question}`); // Muestra la pregunta actual
+			console.log(`Pregunta ${questionCounter + 1}: ${currentQuestion.question}`); 
 		} else {
 			console.log('No hay más preguntas o las preguntas no han sido cargadas.');
 		}
 	}
 
-	// Inicializar la pantalla y establecer la interfaz de usuario
+
 	app.innerHTML = `
 		<h1>OptionsScreen</h1>
 		<p>Responde aquí</p>
@@ -43,32 +43,32 @@ export default function renderOptionsScreen() {
 
 	const continueButton = document.getElementById('continueButton');
 
-	// Escuchar el evento 'prepareToStart' para recibir las preguntas
+
 	socket.on('prepareToStart', (receivedQuestions) => {
-		questions = receivedQuestions; // Guardar las preguntas en el array
-		renderCurrentOptions(); // Renderizar opciones de la primera pregunta
+		questions = receivedQuestions; 
+		renderCurrentOptions();
 		console.log('Preguntas recibidas y renderizadas en OptionsScreen:', questions);
 	});
 
-	// Manejar la selección de una opción
 	document.querySelectorAll('.option').forEach((button) => {
 		button.addEventListener('click', (event) => {
 			answer = ""
-			answer = event.target.value; // Guardar la respuesta seleccionada
+			answer = event.target.value; 
 			console.log(`answer: ${answer}`);
 			continueEnable = true;
-			continueButton.disabled = false; // Habilitar el botón de continuar
+			continueButton.disabled = false; 
 		});
 	});
 
-	// Manejar el clic en el botón de continuar
+
 	continueButton.addEventListener('click', () => {
 		if (continueEnable) {
 
-			socket.emit('saveAnswers', answer, questionCounter);
+			
 			continueEnable = false;
 			continueButton.disabled = true; 
 			questionCounter++; 
+			socket.emit('saveAnswers', answer, questionCounter);
 			renderCurrentOptions(); 
 
 			if (questionCounter >= questions.length) {
