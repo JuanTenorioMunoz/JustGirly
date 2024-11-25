99362abc44f77c83a48f115e27d417290ea04a02
valegaru <valegaru2003@gmail.com>
2024-11-25 10:20:12 -0500

    getting all users

--- server/events-handlers/tvHandlers.js
+++ server/events-handlers/tvHandlers.js
@@ -2,6 +2,7 @@
 
 const { utilFuntion1, utilFuntion2 } = require('../utils/helpers');
 const { users, currentPrompt, currentvs } = require('../db');
+const { getAllUsers } = require('../db/entities/users');
 
 const getQuestionsHandler = (socket, db, io) => {
 	return () => {
@@ -20,7 +21,21 @@ const getVBsHandler = (socket, db, io) => {
 };
 
 const showVBsHandler = (socket, db, io) => {
-	return () => {
+	return async () => {
+		try {
+			// Obtener todos los usuarios desde Supabase
+			const allUsers = await getAllUsers();
+
+			if (allUsers && allUsers.length > 0) {
+				// Emitir los datos obtenidos a los clientes conectados
+				io.emit('showVBs', allUsers);
+				console.log('Usuarios obtenidos y enviados:', allUsers);
+			} else {
+				console.log('No se encontraron usuarios en la base de datos.');
+			}
+		} catch (error) {
+			console.error('Error al obtener usuarios desde Supabase:', error.message);
+		}
 	};
 };
 
